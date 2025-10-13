0902faddfd7a3cd2b3b3e88d6eca60f7b964ab50
Andrea Zambon <azambon@webgriffe.com>
2025-10-13 12:51:02 +0200

    Improves the serializer trait

--- src/RationalSerializerTrait.php
+++ src/RationalSerializerTrait.php
@@ -6,7 +6,7 @@ namespace Webgriffe\Rational;
 
 trait RationalSerializerTrait
 {
-    private function serialize(?Rational $rational): ?string
+    protected function serialize(?Rational $rational): ?string
     {
         if (null === $rational) {
             return null;
@@ -15,22 +15,38 @@ trait RationalSerializerTrait
         [$num, $den] = $rational->getFractionPart();
         $whole = $rational->getWholePart();
 
-        return (string) $whole.':'.(string) $num.':'.(string) $den;
+        return (string) $whole.$this->getSeparator().(string) $num.$this->getSeparator().(string) $den;
     }
 
-    private function unserialize(?string $value): ?Rational
+    protected function unserialize(?string $value): ?Rational
     {
         if (null === $value) {
             return null;
         }
 
-        [$whole, $num, $den] = explode(':', $value);
+        [$whole, $num, $den] = explode($this->getSeparator(), $value);
 
         return Rational::fromWholeAndFraction((int) $whole, (int) $num, (int) $den);
     }
 
-    private function getMaxStringLength(): int
+    protected function getMaxStringLength(): int
     {
+        //The PHP_INT_MIN and PHP_INT_MAX values are both 19 digits long. Adding the possibility of a - sign in front of
+        //each value (even though the denominator should never be negative) brings the figure to 20 chars per number.
+        //Adding the separators that is 20 + 1 + 20 + 1 + 20 = 62 characters, so 64 chars should be more than enough to
+        //store the concatenation of the three values
+
+        //@TODO: make this a constant after we drop support for PHP versions that do not support constants in traits
         return 64;
+    }
+
+    /**
+     * @internal
+     */
+    private function getSeparator(): string
+    {
+        //@TODO: make this a constant and remove this method after we drop support for PHP versions that do not support
+        //      constants in traits
+        return ':';
     }
 }
